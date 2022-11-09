b5101ad540e76d322e47099a9b2d3301391bcd40
Erkan Erol <erkan@giantswarm.io>
2022-11-09 16:47:56 +0300

    Use kustomize to generate helm chart by using upstream repo (#27)
    * Rename files for better review
    
    * Generate with kustomize

---
+++ Makefile.custom.mk
@@ -0,0 +1,30 @@
+##@ App
+
+SHELL := /bin/bash
+
+APPLICATION_NAME="cluster-api-provider-cloud-director"
+
+UPSTREAM_ORG="vmware"
+COMMIT_TO_SYNC="22af653"
+
+.PHONY: all
+all: fetch-upstream-manifest apply-kustomize-patches delete-generated-helm-charts release-manifests ## Builds the manifests to publish with a release (alias to release-manifests)
+
+.PHONY: fetch-upstream-manifest
+fetch-upstream-manifest: ## fetch upstream manifest from
+	# fetch upstream released manifest yaml
+	./hack/sync-version.sh ${UPSTREAM_ORG} ${COMMIT_TO_SYNC}
+
+.PHONY: apply-kustomize-patches
+apply-kustomize-patches: ## apply giantswarm specific patches
+	kubectl kustomize config/kustomize -o config/kustomize/tmp
+
+#.PHONY: delete-generated-helm-charts
+delete-generated-helm-charts: # clean workspace and delete manifests
+	@rm -rvf ./helm/${APPLICATION_NAME}/templates/*.yaml
+	@rm -rvf ./helm/${APPLICATION_NAME}/files/*.yaml
+
+.PHONY: release-manifests
+release-manifests: 
+	# move files from workdir over to helm directury structure
+	./hack/prepare-helmchart.sh ${APPLICATION_NAME}
--- README.md
+++ README.md
@@ -9,6 +9,11 @@ The image is currently built manually from a specific commit since the only rele
 * Repo: https://github.com/vmware/cluster-api-provider-cloud-director/tree/main/config/default
 * Commit: `22af653e351f3e788774fa6000658710b3415706`
 
+### update to a newer CAPVCD release
+
+1. edit the value of `UPSTREAM_ORG` and `COMMIT_TO_SYNC` in [the Makefile](Makefile.custom.mk) to the desired CAPVCD version
+2. run `make all`
+
 ## How to use
 
 There are no credentials in the CAPVCD controller. The credentials are stored in a secret in a namespace that is referenced in the cluster chart.
