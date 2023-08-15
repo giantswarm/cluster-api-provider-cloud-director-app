ed09e02c9fa100fe5aeb347f839dc6ed4adfdc9f
Erkan Erol <erkan@giantswarm.io>
2023-08-15 14:10:46 +0100

    Bump CAPVCD version to 1.1.0 (#64)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Bump CAPVCD version to `1.1.0`.
+
 ### Added
 
 - Add Service monitor cr.
--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -5,7 +5,7 @@ SHELL := /bin/bash
 APPLICATION_NAME="cluster-api-provider-cloud-director"
 
 UPSTREAM_ORG="giantswarm"
-COMMIT_TO_SYNC="a40b68e"
+COMMIT_TO_SYNC="932cf26"
 
 OS ?= $(shell go env GOOS 2>/dev/null || echo linux)
 ARCH ?= $(shell go env GOARCH 2>/dev/null || echo amd64)
