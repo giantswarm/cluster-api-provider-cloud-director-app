ff9285a6f6dfb8c889b29a02f88b58b7ab1eface
Simon Weald <glitchcrab-github@simonweald.com>
2024-03-05 08:50:52 +0000

    update helm chart to 55841ba

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -5,7 +5,7 @@ SHELL := /bin/bash
 APPLICATION_NAME="cluster-api-provider-cloud-director"
 
 UPSTREAM_ORG="giantswarm"
-COMMIT_TO_SYNC="932cf26"
+COMMIT_TO_SYNC="55841ba"
 
 OS ?= $(shell go env GOOS 2>/dev/null || echo linux)
 ARCH ?= $(shell go env GOARCH 2>/dev/null || echo amd64)
